85948926bea9cbd7608d87ab0c7dd4b02efb9378
workInJava <wanghuaminhappy@163.com>
2015-07-26 10:35:47 +0800

    test

--- weiproject/test-wei-project-common/src/main/java/com/test/common/redis/wrapper/JedisManager.java
+++ weiproject/test-wei-project-common/src/main/java/com/test/common/redis/wrapper/JedisManager.java
@@ -1,12 +1,15 @@
 package com.test.common.redis.wrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class JedisManager {
 	
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JedisManager.class);
 	private JedisPool jedisPool;
 
 	public Jedis getJedis() {
